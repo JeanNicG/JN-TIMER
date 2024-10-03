c861e521e6736e57936c55ff07f2c3c56622d16a
Jean-Nicolas Gosselin <jeannicolasgosselin@gmail.com>
2024-10-02 20:52:07 -0400

    first commit base project

---
+++ app/src/main/java/com/example/jntimer/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.jntimer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.jntimer.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'jntimer' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'jntimer' library on application startup.
+        init {
+            System.loadLibrary("jntimer")
+        }
+    }
+}
